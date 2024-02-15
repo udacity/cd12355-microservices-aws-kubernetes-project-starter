33d28892c4a1fd9dc43d8524b64c2cc711e4cbe6
Jay <teguhwpurwanto@gmail.com>
2024-02-15 22:50:59 +0900

    Correct a bug in app.py script - the Token model has not been initialized

--- analytics/app.py
+++ analytics/app.py
@@ -21,7 +21,7 @@ def health_check():
 @app.route("/readiness_check")
 def readiness_check():
     try:
-        count = db.session.query(Token).count()
+        count = db.session.execute(text("SELECT COUNT(*) FROM tokens")).scalar()
     except Exception as e:
         app.logger.error(e)
         return "failed", 500
