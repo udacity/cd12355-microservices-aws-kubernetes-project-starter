f4a43b2a9853e4dc70b3fa0f320e91ba88e549c7
catthanh <catthanh26@gmail.com>
2024-05-11 17:48:45 +0000

    add automatic build

--- analytics/config.py
+++ analytics/config.py
@@ -4,8 +4,8 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db_username = os.environ["DB_USERNAME"]
-db_password = os.environ["DB_PASSWORD"]
+db_username = os.environ.get("DB_USERNAME","myusername")
+db_password = os.environ.get("DB_PASSWORD","mypassword")
 db_host = os.environ.get("DB_HOST", "127.0.0.1")
 db_port = os.environ.get("DB_PORT", "5432")
 db_name = os.environ.get("DB_NAME", "postgres")
